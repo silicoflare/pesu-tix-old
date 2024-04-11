cdcd25ed424fee592d8c8af01598c4eeb0ed0207
Suraj B M <silicoflare@gmail.com>
2024-04-11 23:40:33 +0530

    fix: remove theme button

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import Navbar from "../ui/Navbar";
-import ThemeButton from "../ui/ThemeButton";
 import { inter } from '~/fonts';
 import { Card, CardHeader, CardTitle, CardContent } from '~/components/ui/card';
 import { Badge } from "~/components/ui/badge";
@@ -24,7 +23,7 @@ export default function Dashboard() {
         <div className={`flex flex-col items-center w-screen h-screen bg-background text-primary ${inter}`}>
             <Navbar />
             <div className="w-full flex flex-col items-center mt-5">
-                <h1 className="text-3xl font-semibold text-primary my-5">Your Events</h1>
+                <h1 className="text-3xl font-semibold text-primary my-5 w-3/4 text-left px-7">Dashboard</h1>
                 <div className="p-7 grid grid-cols-3 w-3/4 gap-3">
                     {
                         events.map((e: event) => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { Button } from "~/components/ui/button";
-import ThemeButton from "./ui/ThemeButton";
 
 export default function Home() {
     return (
@@ -11,7 +8,6 @@ export default function Home() {
                 <meta name="description" content="An event ticketing system tailored for PESU" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <ThemeButton />
             <main className="flex flex-col w-screen h-screen items-center justify-center bg-white dark:bg-gray-950">
                 <h1 className="text-5xl text-black dark:text-white font-bold">PESU Tix</h1>
             </main>

288dc9986496e93b9b0b439af0ec275adb0c5c81
Suraj B M <silicoflare@gmail.com>
2024-05-02 08:32:05 +0530

    feat: fetch userdata from session for navbar

--- src/pages/ui/Navbar.tsx
+++ src/pages/ui/Navbar.tsx
@@ -2,40 +2,48 @@ import ThemeButton from './ThemeButton';
 import { TicketIcon } from 'lucide-react';
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator } from '~/components/ui/dropdown-menu';
 import { Avatar, AvatarImage } from '~/components/ui/avatar';
-import { useEffect, useState } from 'react';
+import { Button } from '~/components/ui/button';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { namify } from '~/tools';
 
-export default function Navbar()    {
-    const [ user, setUser ] = useState("");
-
-    useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_URL}/api/user`)
-            .then(data => data.json())
-            .then(json => setUser(json.username));
-    })
+export default function Navbar({ hidelogin = false } : { hidelogin?: boolean })    {
+    const { data: session } = useSession();
+    const router = useRouter();
 
     return (
-        <div className="w-full flex flex-row items-center justify-between px-7 py-4 bg-background text-primary">
+        <div className="w-full flex flex-row items-center justify-between px-7 py-7 basic fixed top-0 left-0 bg-transparent z-0">
             <h1 className="font-semibold text-2xl cursor-pointer flex flex-row gap-x-3 items-center">
-                <TicketIcon className='-rotate-45' />
+                <Link href="/">
+                    <TicketIcon className='-rotate-45' />
+                </Link>
             </h1>
             <span className="flex flex-row items-center justify-end cursor-pointer gap-x-3">
                 <ThemeButton />
-                <DropdownMenu>
-                    <DropdownMenuTrigger className='outline-none'>
-                        <Avatar>
-                            <AvatarImage src={`https://api.dicebear.com/8.x/adventurer-neutral/svg?seed=${user}`}>
-                            </AvatarImage>
-                        </Avatar>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className='mr-5 mt-2'>
-                        <DropdownMenuLabel>{user}</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Foo</DropdownMenuItem>
-                        <DropdownMenuItem>Bar</DropdownMenuItem>
-                        <DropdownMenuItem>Baz</DropdownMenuItem>
-                        <DropdownMenuItem>Boo</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                { !hidelogin && (!!session?.user ? (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className='outline-none'>
+                            <Avatar>
+                                <AvatarImage src={`https://api.dicebear.com/8.x/adventurer-neutral/svg?seed=${namify(session.user.id)}`}>
+                                </AvatarImage>
+                            </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className='mr-5 mt-2'>
+                            <DropdownMenuLabel className='font-semibold text-lg'>{namify(session.user.studentInfo?.name || session.user.clubInfo?.name || session.user.adminInfo?.name || "")}</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Foo</DropdownMenuItem>
+                            <DropdownMenuItem>Bar</DropdownMenuItem>
+                            <DropdownMenuItem>Baz</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem onClick={() => signOut()} className='text-red-600'>Logout</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                ) : (
+                    <Button onClick={() => signIn()}>
+                        Login
+                    </Button>
+                ))}
             </span>
         </div>
     )

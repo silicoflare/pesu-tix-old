321f96910b32020ed19cd8a372146ccdb6ba343b
Suraj B M <silicoflare@gmail.com>
2024-05-02 08:40:04 +0530

    feat: create simple homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Navbar from "./ui/Navbar";
 
 export default function Home() {
     return (
@@ -8,8 +9,8 @@ export default function Home() {
                 <meta name="description" content="An event ticketing system tailored for PESU" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className="flex flex-col w-screen h-screen items-center justify-center bg-white dark:bg-gray-950">
-                <h1 className="text-5xl text-black dark:text-white font-bold">PESU Tix</h1>
+            <main className="flex flex-col w-screen h-screen items-center bg-background">
+                <Navbar />
             </main>
         </>
     );

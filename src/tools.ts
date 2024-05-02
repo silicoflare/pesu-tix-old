44a6819f9081a781d550d36ec3463ba612712e97
Suraj B M <silicoflare@gmail.com>
2024-05-02 08:40:58 +0530

    feat: add function to properly capitalize name

---
+++ src/tools.ts
@@ -0,0 +1,5 @@
+export function namify(name: string)    {
+    let tokens = name.split(' ');
+    tokens = tokens.map(token => token.charAt(0).toUpperCase() + token.slice(1).toLowerCase());
+    return tokens.join(' ');
+}

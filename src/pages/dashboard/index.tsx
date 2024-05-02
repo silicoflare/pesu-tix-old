b3f93d94988b8e0b25df38085d14ddff76332cf0
Suraj B M <silicoflare@gmail.com>
2024-05-02 08:39:16 +0530

    feat: fetch events from db

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,47 +6,66 @@ import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
 import { PlusIcon } from "lucide-react";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { Event } from "~/types";
+import moment from "moment";
+import { api } from "~/utils/api";
+import { useRouter } from "next/router";
+import { useAtom } from "jotai";
+import { editEventID } from "~/atoms";
 
 export default function Dashboard() {
-    const [events, setEvents] = useState<event[]>([]);
+    const { data: session } = useSession();
+    const [events, setEvents] = useState<Event[]>([]);
+    const router = useRouter();
 
-    type event = {
-        name: string,
-        type: 'seminar' | 'workshop' | 'hackathon',
-        date: string
-    }
+    const [editEventId, setEditEvent] = useAtom(editEventID);
 
+    const { data } = api.event.getAllEvents.useQuery({ username: session?.user.id });
     useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_URL}/api/events`)
-            .then(data => data.json())
-            .then(jsonData => setEvents(jsonData.events));
-    }, []);    
+        if (data) {
+            const newData = data.map(x => ({
+                id: x.id,
+                name: x.name,
+                type: x.type as Event["type"],
+                description: x.description,
+                date: x.date,
+                creatorID: x.creatorID,
+            }));
+            setEvents(newData);
+        }
+    }, [data, session]);
+
+    function editEvent(id: string) {
+        setEditEvent(id);
+        router.push("/events/edit");
+    }
 
     return (
         <div className={`flex flex-col items-center w-screen h-screen bg-background text-primary ${inter}`}>
             <Navbar />
             <div className="w-full flex flex-col items-center mt-5">
-                <h1 className="text-3xl font-semibold text-primary my-5 w-3/4 px-7 flex flex-row items-end justify-between">
-                    Dashboard
+                <h1 className="text-3xl font-semibold my-5 mt-20 w-3/4 px-7 flex flex-row items-end justify-between">
+                    Club Dashboard
                     <Button>
-                        <Link href="../events/create" className="flex flex-row items-center gap-x-2">
+                        <Link href="/events/create" className="flex flex-row items-center gap-x-2">
                             <PlusIcon /> Add Event
                         </Link>
                     </Button>
                 </h1>
                 <div className="p-7 grid grid-cols-3 w-3/4 gap-3">
                     {
-                        events.map((e: event) => {
+                        events.map((event: Event) => {
                             return (
-                                <Card key={e.name} className="hover:scale-105 transition duration-200">
+                                <Card className="hover:scale-105 transition duration-200" key={event.id} onClick={() => editEvent(event.id ?? "")}>
                                     <CardHeader>
-                                        <CardTitle>{e.name}</CardTitle>
+                                        <CardTitle>{event.name}</CardTitle>
                                     </CardHeader>
                                     <CardContent className="flex flex-row items-center justify-between">
                                         <span className="text-primary text-lg">
-                                            {e.date}
+                                            {moment(event.date).format("LL \\at LT")}
                                         </span>
-                                        <Badge>{e.type}</Badge>
+                                        <Badge>{event.type}</Badge>
                                     </CardContent>
                                 </Card>
                             )
@@ -57,3 +76,7 @@ export default function Dashboard() {
         </div>
     )
 }
+
+Dashboard.auth = {
+    role: "club"
+};

1a3cc909f5bd38bd1a4093bc519915fdabe2a75e
Suraj B M <silicoflare@gmail.com>
2024-05-25 15:21:47 +0530

    feat(_app): add nhost and react-query providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,22 +9,36 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import "tippy.js/dist/tippy.css";
 import { Toaster } from "~/components/ui/toaster";
+import { NhostProvider } from "@nhost/nextjs";
+import { nhost } from "~/tools";
+import { createContext } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const QueryClientContext = createContext<QueryClient | null>(null);
+
 
 const MyApp: AppType<{ session: Session | null }> = ({
     Component,
     pageProps: { session, ...pageProps },
 }) => {
+    const queryClient = new QueryClient();
     return (
-        <SessionProvider session={session}>
-            <Toaster />
-            {(Component as any).auth ? (
-                <Auth role={(Component as any).auth.role}>
-                    <Component {...pageProps} />
-                </Auth>
-            ) : (
-                <Component {...pageProps} />
-            )}
-        </SessionProvider>
+        <QueryClientProvider client={queryClient}>
+            <QueryClientContext.Provider value={queryClient}>
+                <SessionProvider session={session}>
+                    <NhostProvider nhost={nhost}>
+                        <Toaster />
+                        {(Component as any).auth ? (
+                            <Auth role={(Component as any).auth.role}>
+                                <Component {...pageProps} />
+                            </Auth>
+                        ) : (
+                            <Component {...pageProps} />
+                        )}
+                    </NhostProvider>
+                </SessionProvider>
+            </QueryClientContext.Provider>
+        </QueryClientProvider>
     )
 };
 

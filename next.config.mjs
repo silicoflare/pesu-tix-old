6301d33cde963ff0cc989ff15b0c21f790bf32ce
Suraj B M <silicoflare@gmail.com>
2024-05-25 15:14:36 +0530

    build: allow nhost thru nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,14 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drtrwkdxomylrqnhlehb.storage.ap-south-1.nhost.run",
+      }
+    ]
+  }
 };
 
 export default config;
